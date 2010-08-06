8bd64e8525352449bb0406928439ac79155ca438
Flurin Egger <flurin@digitpaint.nl>
2010-08-06 19:06:02 +0200

    Correctly implement proxy options

--- lib/resource.rb
+++ lib/resource.rb
@@ -6,7 +6,7 @@ module RestSourcery
       base.send(:attr_reader, :url, :attributes, :errors)
       base.send(:mattr_inheritable, :scope, :associations, :property_map)
       base.instance_variable_set("@property_map", {})
-      base.instance_variable_set("@associations", [])
+      base.instance_variable_set("@associations", {})
       base.instance_variable_set("@scope", [])
       
       # This has to be done like this otherwise we have ruby
@@ -83,20 +83,22 @@ module RestSourcery
       # 
       # ==== Options
       # :klass<Class>:: The class that will be used for the elements of this collection
+      # :included<Boolean>:: Is this a nested collection? If it is it will always be included in the parent when storing
       # --
-      def has_many(collection_name,options={})
+      def has_many(collection_name, options={})
         class_name = (options[:klass] || collection_name.to_s.singularize.classify).to_s
+        proxy_options = options
     
         # Register association
-        self.associations ||= []
-        self.associations << collection_name if !self.associations.include?(collection_name)
+        self.associations ||= {}
+        self.associations[collection_name.to_s] = options if !self.associations.has_key?(collection_name)
     
         # Define methods
         methods = <<-EOS
           def #{collection_name}(options={}) 
-            @#{collection_name} ||= ResourceCollectionProxy.new(self,#{class_name},options)
+            @#{collection_name} ||= ResourceCollectionProxy.new(self, #{class_name}, options, self.class.associations["#{collection_name}"])
           end
-      
+              
           def #{collection_name}=(v)
             self.#{collection_name}.load(v)
           end
@@ -357,9 +359,9 @@ module RestSourcery
   
         attrs.to_xml(options.merge(:root => self.resource_name)) do |builder|
           if self.class.associations
-            self.class.associations.each do |assoc_name|
+            self.class.associations.each do |assoc_name,options|
               collection_proxy = self.send(assoc_name)
-              if collection_proxy.included?
+              if collection_proxy.included? && collection_proxy.size > 0
                 collection_proxy.to_xml(:builder => builder, :root => assoc_name.to_s, :skip_instruct => true)
               end
             end
